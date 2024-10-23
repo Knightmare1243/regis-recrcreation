232211d1d76df6392acccbb3abd619753699b69a
Knightmare1243 <regiscc07@yahoo.com>
2024-10-23 16:28:21 +0100

    Steps for the tokens and their display using chat GPT. Token made by Aesprite or me.

--- Connectfour/script.js
+++ Connectfour/script.js
@@ -1,10 +1,20 @@
 var buttons = document.getElementsByClassName("btn");
 var total = 0
+
+// Single image URL
+var image = "Token 1.gif"; // Set a default image URL
+
 for(var i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function(){
         //buttons[i].classList.add("btn-player-1");
         total = total+1;
-        window.alert("this is "+ total);
+        var buttonClass = this.className; // `this` refers to the clicked button
+        var number = buttonClass.match(/btn-(\d+)/)[1]; // Extracts the number using regex
+        var displayImage = this.querySelector("img");
+            displayImage.src = image
+            displayImage.style.display = 'block'; // Show the image
+        
+        window.alert("You pressed " + number + ". Total clicks: " + total);
     });
 }
 //odd number = player 1 ; even number player 2
