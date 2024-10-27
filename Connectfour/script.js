c07e4cc69f62533da96a9c9e323046686c62c61f
Knightmare1243 <regiscc07@yahoo.com>
2024-10-27 11:22:31 +0000

    Progress on player's turns for ConnectFour

--- Connectfour/script.js
+++ Connectfour/script.js
@@ -2,19 +2,22 @@ var buttons = document.getElementsByClassName("btn");
 var total = 0
 
 // Single image URL
-var image = "Token 1.gif"; // Set a default image URL
-
+var image = ["Token 1.gif","Token 2.gif"]; // Set a default image URL
+var innertext = "player "
 for(var i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function(){
         //buttons[i].classList.add("btn-player-1");
-        total = total+1;
+        
+        
         var buttonClass = this.className; // `this` refers to the clicked button
         var number = buttonClass.match(/btn-(\d+)/)[1]; // Extracts the number using regex
         var displayImage = this.querySelector("img");
-            displayImage.src = image
+            displayImage.src = image[total %2]
             displayImage.style.display = 'block'; // Show the image
         
         window.alert("You pressed " + number + ". Total clicks: " + total);
+        total = total+1;
+        document.getElementById("myHeader").innerText = innertext+(total %2 +1);
     });
 }
 //odd number = player 1 ; even number player 2
